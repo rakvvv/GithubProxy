a94c89390c5e57273211e7debfcc686635511d39
Rafał <rl125140@stud.ur.edu.pl>
2026-06-18 11:58:57 +0200

    Initial Commit: Project structure

---
+++ src/main/java/pl/rliszcz/githubproxy/GithubProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.rliszcz.githubproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubProxyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/rliszcz/githubproxy/GithubRepositoryController.java
@@ -0,0 +1,9 @@
+package pl.rliszcz.githubproxy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class GithubRepositoryController {
+}
